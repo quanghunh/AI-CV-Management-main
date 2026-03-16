bd110da03e8f8161b00edae106ba5833a32009b7
Hậu Nguyễn <147897394+leobigboy@users.noreply.github.com>
2026-03-16 05:12:33 +0000

    Update Supabase config and fixes

--- AI-CV-Management-main/src/lib/supabaseClient.ts
+++ AI-CV-Management-main/src/lib/supabaseClient.ts
@@ -1,7 +1,7 @@
 // src/lib/supabaseClient.ts
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://supabase.softworld.dev'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlzcyI6InN1cGFiYXNlIiwiaWF0IjoxNzU1NDUwMDAwLCJleHAiOjE5MTMyMTY0MDB9.Mpdx1os5ZzrDzI_Yq3GzVE33Hw-iALkowFrm902qlbw'
+const supabaseUrl = 'https://aeotpgcvonuoosztjlfa.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFlb3RwZ2N2b251b29zenRqbGZhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMwMTQxNzYsImV4cCI6MjA4ODU5MDE3Nn0.AvFOtybDTVDgdGRMxUirgH9yU2ZA6fPc6s-12KI-mWQ'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
